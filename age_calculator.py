808e19fdafddad5a150f819c496a44a42bf2b2d9
bwritam <bwritam@gmail.com>
2021-01-29 17:32:37 +0530

    initial commit

---
+++ age_calculator.py
@@ -0,0 +1,9 @@
+# this programme will return the year when you'll turn 100.
+# practice 1:
+
+def person(name, age):
+	years = (2021 - age) + 100
+	print(f"{name} you are now {age} years old. You will turn 100 years old in the year {years}.")
+
+
+person("Tom", 23)
