ee93bf6c92a8987258fe8198e8ea59c1322fdb01
bwritam <bwritam@gmail.com>
2021-01-30 22:29:54 +0530

    separate line messages

--- age_calculator.py
+++ age_calculator.py
@@ -1,5 +1,7 @@
 # this programme will return the year when you'll turn 100.
 # taking input for that many copies.
+# copies of the previous message on separate lines.
+
 # practice 1:
 
 def person():
@@ -7,7 +9,7 @@ def person():
 	age = int(input("Enter your age: "))
 	years = (2021 - age) + 100
 	copies = int(input("How many copies do you want? "))
-	print(f"{name} you are now {age} years old. You will turn 100 years old in the year {years}." * copies)
+	print(f"{name} you are now {age} years old. You will turn 100 years old in the year {years}.\n" * copies)
 
 
 person()
