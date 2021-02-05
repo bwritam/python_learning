f8849d3c75235a2e0619f80dcca047d0d14a0e31
bwritam <bwritam@gmail.com>
2021-02-05 10:58:46 +0530

    list overlap, randomly generate lists

--- list_duplicates.py
+++ list_duplicates.py
@@ -1,10 +1,35 @@
-# Without duplicates lists
-# Practice: 5 (initial)
+# Without duplicates lists, list overlap.
+# Practice: 5
+def two_list(list_1, list_2):
+
+	list_3 = []
+	for i in list_1:
+		if i in list_2:
+			if i not in list_3:
+				list_3.append(i)
+	return list_3
+
+
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-c = []
-for i in a:
-	if i in b:
-		if i not in c:
-			c.append(i)
-print(c)
+print(two_list(a,b))
+
+
+# Randomly generate two lists to test this.
+import random as rd
+
+def rndm_list():
+
+	list_1 = rd.sample(range(1, 100), rd.randint(5, 20))
+	list_2 = rd.sample(range(50, 100), rd.randint(10, 15))
+	list_3 = []
+
+	for i in list_1:
+		if i in list_2:
+			if i not in list_3:
+				list_3.append(i)
+	return list_3
+
+
+in_list = rndm_list()
+print(in_list)
