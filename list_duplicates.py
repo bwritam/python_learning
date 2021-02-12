ed6a9bcd3a20621b4d888e9376aa3df31d8ea470
bwritam <bwritam@gmail.com>
2021-02-12 10:43:35 +0530

    d_listduplicates_sets_loops

--- list_duplicates.py
+++ list_duplicates.py
@@ -33,3 +33,16 @@ def rndm_list():
 
 in_list = rndm_list()
 print(in_list)
+
+
+#List overlap using sets
+def ovrlp_st(list1, list2):
+
+	nwst1 = set(list1)
+	nwst2 = set(list2)
+	nwst3 = nwst1.intersection(nwst2)
+	return list(nwst3)
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+print(ovrlp_st(a, b))
