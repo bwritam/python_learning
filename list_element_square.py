71416450662f9f1f88b05f3259f0304cd7a75d2a
bwritam <bwritam@gmail.com>
2021-02-21 11:20:05 +0530

    initial

---
+++ list_element_square.py
@@ -0,0 +1,16 @@
+list_1 = [1, 2, 3, 4, 5, 6]
+
+
+# make a new list whose each values are square of the old-list
+# for example: new list would be: [1, 4, 9, 16, 25, 36]
+
+
+# METHOD-1: List comprehension_________________________________________________
+
+# create the required list
+list_2 = [x * x for x in list_1]
+
+# check the new list
+print (list_2)
+
+
