cf8393aeb920cabbbd12ab1c004cf9ede286781d
bwritam <bwritam@gmail.com>
2021-03-02 10:21:00 +0530

    sum of text digits and put it into a new file & some of new uses of function

--- file_handling_2.py
+++ file_handling_2.py
@@ -1,16 +1,17 @@
 
-# # Take some numbers from a text file and put the sum of this numbers to a new text file.
-# f = open(r"C:\Users\Anik\Desktop\demofile.txt", "r")
+# Take some numbers from a text file and put the sum of this numbers to a new text file.
+f = open(r"C:\Users\Anik\Desktop\demofile.txt", "r")
 
-# c = 0
-# for i in f:
-# 	c = c + int(i)
-# print(c)
+c = 0
+for i in f:
+	c = c + int(i)
+print(c)
 
-# f = open(r"C:\Users\Anik\Desktop\taskfile.txt", "w")
-# f.write(str(c))
-# f.close()
+f = open(r"C:\Users\Anik\Desktop\taskfile.txt", "w")
+f.write(str(c))
+f.close()
 
+# Unknown length of a function argument
 def arg(*x):
 	c = 0
 	for i in x:
