e8379d64da85ba90c50cb80c197ff254f6bce9b1
bwritam <bwritam@gmail.com>
2021-03-02 10:16:46 +0530

    sum of text digits

---
+++ file_handling_2.py
@@ -0,0 +1,25 @@
+
+# # Take some numbers from a text file and put the sum of this numbers to a new text file.
+# f = open(r"C:\Users\Anik\Desktop\demofile.txt", "r")
+
+# c = 0
+# for i in f:
+# 	c = c + int(i)
+# print(c)
+
+# f = open(r"C:\Users\Anik\Desktop\taskfile.txt", "w")
+# f.write(str(c))
+# f.close()
+
+def arg(*x):
+	c = 0
+	for i in x:
+		c = c + i
+
+	print (c)
+
+
+arg(5, 6, 1, 3)
+
+
+
