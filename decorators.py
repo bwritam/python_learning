d1f1489d7c1de7eabdeced4aa80499172a55ed20
bwritam <bwritam@gmail.com>
2021-03-12 10:40:31 +0530

    initial noob decorators

---
+++ decorators.py
@@ -0,0 +1,17 @@
+
+# Decorators, call a function between a function.
+def div(a, b):
+	print(a/b)
+
+def smart_div(func):
+
+	def inner_div(a, b):
+		if a < b:
+			a,b = b,a
+		return func(a,b)
+	return inner_div
+
+
+div = smart_div(div)
+div(4,8)
+
