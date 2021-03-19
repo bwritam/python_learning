0561e86e1a4adbeb5dd70fd25604369884875f73
bwritam <bwritam@gmail.com>
2021-03-19 09:46:19 +0530

    image file handling

--- initial_filehandling.py
+++ initial_filehandling.py
@@ -1,6 +1,17 @@
-import os
+import os # only use when you need to delete any file
 
-f = open(r"C:\Users\Anik\Desktop\demofile.txt", "r")
-print(f.readline())
+f = open(r"C:\Users\Anik\Desktop\demofile.txt", "r") # you have to mention the file path if it is not the right python files
+print(f.readline()) # readline is used for read the first line
 
-f. close()
+f. close() # after openning file always close it, its a good practice
+
+
+#################################################################
+#################################################################
+
+f = open(r"C:\Users\tomlini\Desktop\DSC_0105.JPG", "rb") # when you want to read a image use rb, read binary
+
+f1 = open(r"C:\Users\tomlini\Desktop\copy_dsc.JPG", "wb") # you can copy a image using wb, write binary
+
+for i in f:
+	print(i)
